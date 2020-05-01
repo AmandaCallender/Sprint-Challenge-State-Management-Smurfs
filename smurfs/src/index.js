b607ad7326591debc91374a5e40a86b959789b31
Amanda-Callender <amanda.callender@outlook.com>
2020-05-01 19:31:39 -0400

    index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { reducer } from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById("root")
+    );
